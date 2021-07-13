432fd118816a2f5c9a8e00794d3b56d5fd6841e7
tomoki tanaka <tomoki.tanaka@sun-asterisk.com>
2021-07-13 14:29:28 +0900

    add showfirestoredata method

--- lib/show_firestore_data.dart
+++ lib/show_firestore_data.dart
@@ -1,23 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ShowFirestoreData extends StatelessWidget {
+class ShowFirestoreData extends StatefulWidget {
+  @override
+  _MyFirestorePageState createState() => _MyFirestorePageState();
+}
+
+class _MyFirestorePageState extends State<ShowFirestoreData> {
+  List<DocumentSnapshot> documentList = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'firestoreのデータ表示',
-        ),
-      ),
       body: Center(
-        child: RaisedButton(
-          child: Text('前のページに戻るよ'),
-          onPressed: () {
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            RaisedButton(
+              child: Text('Show Data'),
+              onPressed: () async {
+                final snapshot = await Firestore.instance
+                    .collection('user')
+                    .getDocuments();
+                setState(() {
+                  documentList = snapshot.documents;
+                });
+              },
+            ),
+            Column(
+              children: documentList.map((document) {
+                return ListTile(
+                  title: Text('name:${document['name']}'),
+                );
+              }).toList(),
+            ),
+            Container(
+                child: RaisedButton(
+              child: Text('前のページに戻るよ'),
+              onPressed: () {
 // -*--*--*--*--*- Navigator -*--*--*--*--*-
-            Navigator.pop(context);
+                Navigator.pop(context);
 // -*--*--*--*--*- Navigator -*--*--*--*--*-
-          },
+              },
+            ))
+          ],
         ),
       ),
     );
